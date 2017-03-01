0069d3d9c0e709d76234e49245bd8b1c6d1dcd04
Christian Stigen Larsen <csl@csl.name>
2017-03-02 00:00:32 +0100

    Adds Position

--- cython/arv.pyx
+++ cython/arv.pyx
@@ -7,6 +7,7 @@ from libcpp cimport bool
 from libcpp.string cimport string
 
 cdef extern from "dnatraits.hpp":
+    ctypedef uint32_t Position
     ctypedef uint32_t RSID
 
     cdef cppclass Genome:
