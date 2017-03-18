cf75b97ea5a8e6270bd74a20876dff973e48d3a0
Christian Stigen Larsen <csl@csl.name>
2017-03-18 19:23:21 +0100

    Uses lazy_cythonize

--- setup.py
+++ setup.py
@@ -1,57 +1,52 @@
-from Cython.Build import cythonize
-from distutils.core import setup, Extension
-#from setuptools import setup, Extension
-import Cython
+from setuptools import setup, Extension
 import multiprocessing
 import os
 import shutil
 
-print("Using Cython %s" % Cython.__version__)
+# From http://stackoverflow.com/a/26698408/21028
+class lazy_cythonize(list):
+    def __init__(self, callback):
+        self._list, self.callback = None, callback
+    def c_list(self):
+        if self._list is None: self._list = self.callback()
+        return self._list
+    def __iter__(self):
+        for e in self.c_list(): yield e
+    def __getitem__(self, ii): return self.c_list()[ii]
+    def __len__(self): return len(self.c_list())
 
 def configure_google_hashmap():
-    # TODO: In temp dir or something
+    # TODO: Perform out-of-source
     print("Configuring Google hash map")
     if os.system("3rd-party/sparsehash/configure") != 0:
         raise RuntimeError("Error configuring Google hash map")
     shutil.copy("src/config.h", "cpp/sparsehash/internal/sparseconfig.h")
 
+def extensions():
+    from Cython.Build import cythonize
+    import multiprocessing
+
+    exts = [
+        Extension("_arv", [
+                "cpp/dnatraits.cpp",
+                "cpp/file.cpp",
+                "cpp/fileptr.cpp",
+                "cpp/filesize.cpp",
+                "cpp/mmap.cpp",
+                "cpp/parse_file.cpp",
+                "cython/_arv.pyx",
+            ],
+            language="c++",
+            include_dirs=["cpp"],
+            extra_compile_args=["-std=c++11", "-g0"], # gcc specific
+        ),
+    ]
+    configure_google_hashmap()
+    return cythonize(exts, nthreads=multiprocessing.cpu_count())
+
 def slurp(filename):
     with open(filename, "rt") as f:
         return f.read()
-
-configure_google_hashmap()
-
-extensions = [
-    Extension("_arv", [
-            "cpp/dnatraits.cpp",
-            "cpp/file.cpp",
-            "cpp/fileptr.cpp",
-            "cpp/filesize.cpp",
-            "cpp/mmap.cpp",
-            "cpp/parse_file.cpp",
-            "cython/_arv.pyx",
-        ],
-        language="c++",
-        include_dirs=["cpp"],
-        extra_compile_args=["-std=c++11", "-g0"], # gcc specific
-    ),
-]
-
-keywords = [
-    "23andMe",
-    "bio",
-    "biology",
-    "biopython",
-    "disease",
-    "DNA",
-    "gene",
-    "genome",
-    "health",
-    "protein",
-    "RNA",
-    "RSID",
-    "SNP",
-]
 
 setup(
     name="arv",
@@ -64,7 +59,22 @@ setup(
     url="https://github.com/cslarsen/arv",
     license="https://www.gnu.org/licenses/gpl-3.0.html",
     long_description=slurp("README.rst"),
-    keywords=keywords,
+    keywords=[
+        "23andMe",
+        "bio",
+        "biology",
+        "biopython",
+        "disease",
+        "DNA",
+        "gene",
+        "genome",
+        "health",
+        "protein",
+        "RNA",
+        "RSID",
+        "SNP",
+    ],
     platforms=["unix", "linux", "osx"],
-    ext_modules=cythonize(extensions, nthreads=multiprocessing.cpu_count()),
+    install_requires=["cython>=0.25"],
+    ext_modules=lazy_cythonize(extensions),
 )
