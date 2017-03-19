12ba43366e7f0a110398bd52064422158f7ff426
Christian Stigen Larsen <csl@csl.name>
2017-03-19 17:55:09 +0100

    Uses system python

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 set -e
-WHEEL_TOOL=`which wheel` python setup.py sdist bdist_wheel
+WHEEL_TOOL=`which wheel` /usr/bin/python2.7 setup.py sdist bdist_wheel
 find dist -type f -exec gpg2 --detach-sign -a {} \;
 twine upload dist/*
