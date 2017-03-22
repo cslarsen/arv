3934e97a41f35925ee96530a75b9993383dccc4e
Christian Stigen Larsen <csl@csl.name>
2017-03-22 22:04:09 +0100

    Bumps to v0.6

--- arv/__init__.py
+++ arv/__init__.py
@@ -68,7 +68,7 @@ __email__ = "csl@csl.name"
 __license__ = "GNU General Public License v3 or later"
 __maintainer__ = "Christian Stigen Larsen"
 __status__ = "Prototype"
-__version__ = "0.5"
+__version__ = "0.6"
 
 __all__ = [
     "_sizes",
--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ def get_testsuite():
 setup(
     name="arv",
     packages=["arv"],
-    version="0.5",
+    version="0.6",
     description="A fast 23andMe raw genome file parser",
     author="Christian Stigen Larsen",
     author_email="csl@csl.name",
