6d63e4fef4cf0d1f0a5da13d3d042a5b2b3c935a
Christian Stigen Larsen <csl@csl.name>
2017-03-28 10:23:10 +0200

    Bumps to v0.9

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ def get_testsuite():
 setup(
     name="arv",
     packages=["arv"],
-    version="0.8",
+    version="0.9",
     description="A fast 23andMe raw genome file parser",
     author="Christian Stigen Larsen",
     author_email="csl@csl.name",
